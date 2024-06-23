ea6eaa83482d2e6dfc44dead5cdea3d5f52e1feb
admiswalker <admiswalker@gmail.com>
2024-06-23 23:23:28 +0900

    wip

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-
+import yaml
 
 def main():
     print('hello')
+    with open('test.yaml') as fp:
+        obj = yaml.safe_load(fp)
+        print(obj)
+        print('obj[0]')
+        print(obj[0])
+        print('obj[1]')
+        print(obj[1])
 
 main()
