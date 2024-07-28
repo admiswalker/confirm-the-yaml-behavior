463bcafffa3abf31e6c39861e4322549f09465c4
admiswalker <admiswalker@gmail.com>
2024-07-28 23:23:28 +0900

    wip

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def main():
     with open('test.yaml') as fp:
         obj = yaml.safe_load(fp)
         print(obj)
-        print('obj[0]')
-        print(obj[0])
-        print('obj[1]')
-        print(obj[1])
+        #print(obj['listX'][1])
+        #print('obj[0]')
+        #print(obj[0])
+        #print('obj[1]')
+        #print(obj[1])
 
 main()
